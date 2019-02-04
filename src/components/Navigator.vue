3e94e23a5d7192b78174936c273724aebd584364
clooooode <jackey8616@gmail.com>
2019-02-04 21:30:58 +0800

    Add two-level navigator & Fxxk your Lanar New Year
    Add BS4 support two-level navigator with stages and lessons.
    Add missing &nbsp; at Index platform detail section.

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -5,19 +5,25 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <div class="navbar-nav">
-        <router-link to="/" class="nav-item nav-link">Index</router-link>
-        <a v-if="stages === {}" class="nav-item nav-link">Stage Loading</a>
-        <li class="nav-item dropdown" v-for="(value, key) in stages" :key="key">
-          <a @click="fetchLesson(key)" class="nav-link dropdown-toggle" href="#" id="dropdownAbout" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Stage {{ key }}</a>
-          <div v-if="stages[key].lessons === undefined" class="dropdown-menu" aria-labelledby="dropdownAbout">
-            <a class="dropdown-item">Loading...</a>
-          </div>
-          <div v-else class="dropdown-menu" aria-labelledby="dropdownAbout">
-            <router-link v-for="lesson in stages[key].lessons" :key="lesson.index" :to="lesson.url" class="dropdown-item">{{ lesson.title }}</router-link>
-          </div>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Index</router-link>
         </li>
-      </div>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" id="stageDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Stage</a>
+          <ul class="dropdown-menu" aria-labelledby="stageDropdown">
+            <li v-for="(value, key) in stages" :key="key" class="dropdown-submenu">
+              <a @mouseover="fetchLesson(key)" class="dropdown-item dropdown-toggle">{{ key }}</a>
+              <ul class="dropdown-menu">
+                <li v-if="stages[key].lessons === undefined"><a class="dropdown-item disable">Lesson Loading...</a></li>
+                <li v-else v-for="lesson in stages[key].lessons" :key="lesson.index">
+                  <router-link :to="lesson.url" class="dropdown-item">{{ lesson.title }}</router-link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
@@ -52,17 +58,19 @@ export default {
       })
     },
     fetchLesson: function (stageName) {
-      this.$ajax({
-        'method': 'GET',
-        'url': this.$backend + '/stage/' + stageName
-      }).then(response => {
-        var data = response.data.data
-        this.$set(this.stages[stageName], 'lessons', Object.values(data).sort((a, b) => {
-          return parseInt(a.index) - parseInt(b.index)
-        }))
-      }).catch(error => {
-        console.log(error)
-      })
+      if (this.stages[stageName].lessons === undefined) {
+        this.$ajax({
+          'method': 'GET',
+          'url': this.$backend + '/stage/' + stageName
+        }).then(response => {
+          var data = response.data.data
+          this.$set(this.stages[stageName], 'lessons', Object.values(data).sort((a, b) => {
+            return parseInt(a.index) - parseInt(b.index)
+          }))
+        }).catch(error => {
+          console.log(error)
+        })
+      }
     }
   }
 }
@@ -73,5 +81,21 @@ export default {
     background-color: #fa561e;
     box-sizing: border-box;
     position: relative;
+  }
+  .navbar-nav li:hover > ul.dropdown-menu {
+    display: block;
+  }
+  .dropdown-submenu {
+    position:relative;
+  }
+  .dropdown-submenu>.dropdown-menu {
+    top:0;
+    left:100%;
+    margin-top:-6px;
+  }
+  /* rotate caret on hover */
+  .dropdown-menu > li > a:hover:after {
+    text-decoration: underline;
+    transform: rotate(-90deg);
   }
 </style>
